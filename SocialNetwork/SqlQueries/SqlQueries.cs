a328811ce6b1eae37f93c033e1a775d7f5d07826
thevolumebot <thevolumebot@gmail.com>
2019-07-08 10:46:14 +0200

    Log in system MVP implemented

--- SocialNetwork/SqlQueries/SqlQueries.cs
+++ SocialNetwork/SqlQueries/SqlQueries.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace SocialNetwork
 {
     class SqlQueries
     {
+        private static string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SocialNetworkApp;Integrated Security=True";
+
+        public bool TestLogInInfo(string nickName, string password)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlConnection.Open();
+                string sql = $"SELECT * FROM AppUser Where AppUser.NickName = '{nickName}' AND AppUser.UserPassword = '{password}'";
+                sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = sql;
+                var reader = sqlCommand.ExecuteReader();
+                if (reader.Read())
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            
+            }
+    
+
+        }
     }
 }
