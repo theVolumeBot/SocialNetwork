2fe7525dddcc7898dbc763669b66e3177f481f0c
Miisha87 <michaela.wallin@hotmail.com>
2019-07-08 14:10:31 +0200

    Added CreateUser to SQLQuerys

--- SocialNetwork/SqlQueries/SqlQueries.cs
+++ SocialNetwork/SqlQueries/SqlQueries.cs
@@ -59,5 +59,22 @@ namespace SocialNetwork
 
             return user;
         }
-    }
+
+        public static void CreateUser(string nickName, string userPassword, DateTime birthDate, string city, string secret)
+        {
+            User user = new User();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string sqlString = $"INSERT INTO AppUser (NickName, UserPassword, Birthdate, City, [Secret]) VALUES ('{nickName}', '{userPassword}', '{birthDate}', '{city}', '{secret}')";
+
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.ExecuteNonQuery(); 
+
+
+            }
+
+        }
 }
